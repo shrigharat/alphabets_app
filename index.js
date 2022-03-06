bee147a26a833057943b31e51c892ee74ab9fe43
Shrishail Gharat <gharatshri014@gmail.com>
2022-03-07 01:52:16 +0530

    Added browser text to speech synthesis

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 var buttons = document.querySelectorAll(".alpha-btn");
 var display = document.getElementById("pressed-display");
 
-for(i = 0 ; i < buttons.length ; i++ ) {
-     buttons[i].addEventListener("click", function() {
-       this.style.color = "white";
-       var keyWord = this.innerHTML
-       onClickButton(keyWord);
-       buttonAnimation(keyWord);
-     }
-   )
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    this.style.color = "white";
+    var keyWord = this.innerHTML;
+    onClickButton(keyWord);
+    buttonAnimation(keyWord);
+  });
 }
 
-document.addEventListener("keypress", function(event) {
+document.addEventListener("keypress", function (event) {
   onClickButton(event.key);
   buttonAnimation(event.key);
-})
+});
 
 function onClickButton(keyWord) {
-  var audio = new Audio(`sounds/${keyWord.toLowerCase()}.mp3`);
+  // var audio = new Audio(`sounds/${keyWord.toLowerCase()}.mp3`);
   display.innerHTML = "";
   display.innerHTML = keyWord.toUpperCase();
-  audio.play();
+  let synth = new SpeechSynthesisUtterance(keyWord);
+  synth.rate = 0.4;
+  window.speechSynthesis.speak(synth);
+  // audio.play();
 }
 
 function buttonAnimation(currentKey) {
-  var button = document.querySelector('.btn-' + currentKey.toLowerCase())
-  button.classList.add('pressed');
-  setTimeout(() => { button.classList.remove('pressed'); }, 500);
+  var button = document.querySelector(".btn-" + currentKey.toLowerCase());
+  button.classList.add("pressed");
+  setTimeout(() => {
+    button.classList.remove("pressed");
+  }, 500);
 }
